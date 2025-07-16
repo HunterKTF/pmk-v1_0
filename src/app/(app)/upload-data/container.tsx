20d9b9aa51a441573303c01292bbdf92c18d0a40
Jorge R. Hernández Sabino <jorgehernandez336@gmail.com>
2025-07-16 00:45:32 -0600

    added upload data layout

--- src/app/(app)/upload-data/container.tsx
+++ src/app/(app)/upload-data/container.tsx
@@ -2,9 +2,21 @@
 
 export default function UploadDataContainer(): React.JSX.Element {
   return (
-    <div className="flex flex-col items-center justify-center h-full">
-      <h1 className="text-2xl font-bold mb-4">Upload Data Container</h1>
-      <p className="text-gray-600">This is a placeholder for the upload data container.</p>
+    <div className="w-full h-full flex flex-col gap-2 p-4">
+      <div className="w-full h-[26%] grid grid-cols-4 gap-2">
+        <div className="w-full h-full bg-teal-600 rounded-md"></div>
+        <div className="w-full h-full bg-teal-600 rounded-md"></div>
+        <div className="w-full h-full bg-teal-600 rounded-md"></div>
+        <div className="w-full h-full bg-teal-600 rounded-md"></div>
+      </div>
+      <h6 className="text-sm font-bold">Recent Files</h6>
+      <div className="w-full h-[18%] grid grid-cols-3 gap-2">
+        <div className="w-full h-full bg-teal-600 rounded-md"></div>
+        <div className="w-full h-full bg-teal-600 rounded-md"></div>
+        <div className="w-full h-full bg-teal-600 rounded-md"></div>
+      </div>
+      <h6 className="text-sm font-bold">All Files</h6>
+      <div className="w-full h-full bg-teal-600 rounded-md"></div>
     </div>
   )
 }
